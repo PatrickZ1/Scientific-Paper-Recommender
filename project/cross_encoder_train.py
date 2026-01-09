f8313a66f960b335801a05b9ed9fc50955749b40
Patrick Ziegler <patrick.ziegler@student.tugraz.at>
2026-01-09 16:14:49 +0100

    added TensorBoard with Small Eval-Set

--- project/cross_encoder_train.py
+++ project/cross_encoder_train.py
@@ -3,6 +3,7 @@ from sentence_transformers.cross_encoder import (
     CrossEncoderTrainer,
     CrossEncoderTrainingArguments,
     losses,
+    evaluation,
 )
 from sentence_transformers.training_args import BatchSamplers
 
@@ -13,7 +14,11 @@ BASE_MODEL = "cross-encoder/ms-marco-TinyBERT-L2-v2"
 MODEL_OUT_DIR = pathlib.Path("./models/cross_encoder") / "tinybert_scidocs_cite"
 CHECKPOINT_DIR = pathlib.Path("./.cross_enc_chkpt")
 
+# Number of pairs from the evaluation set to use for evaluation during training
+NUM_EVAL_PAIRS = 5000
+
 # NOTE: Adjust if using a GPU with more/less memory
+# NOTE: If you adjust this, adjust eval_steps and logging_steps in the training args accordingly
 BATCH_SIZE = 40
 
 MODEL_OUT_DIR.mkdir(parents=True, exist_ok=True)
@@ -21,11 +26,34 @@ CHECKPOINT_DIR.mkdir(parents=True, exist_ok=True)
 
 model = CrossEncoder(BASE_MODEL, device="cuda")
 
+scidocs_cite = load_scidocs_cite()
+
 # TODO: use more than 1/100 of the training data for quick testing
 train_dataset = scidoc_cite_to_train_triplets(
     filter_scidocs_cite(
-        load_scidocs_cite()["train"].shard(100, 0), load_relish()["evaluation"]
+        scidocs_cite["train"].shard(100, 0), load_relish()["evaluation"]
     )
+)
+
+# NOTE: Due to computational constraints, we use only a subset of the evaluation set for evaluation during training (5000 pairs take around 2sec on my machine)
+eval_pairs = []
+eval_labels = []
+for item in tqdm(
+    scidocs_cite["validation"].shuffle(seed=42).select(range(NUM_EVAL_PAIRS // 2)),
+    desc="Preparing evaluation pairs",
+):
+    query = item["query"]["title"] + " " + (item["query"]["abstract"] or "")
+    pos = item["pos"]["title"] + " " + (item["pos"]["abstract"] or "")
+    neg = item["neg"]["title"] + " " + (item["neg"]["abstract"] or "")
+    eval_pairs.append([query, pos])
+    eval_labels.append(1)
+    eval_pairs.append([query, neg])
+    eval_labels.append(0)
+
+evaluator = evaluation.CrossEncoderClassificationEvaluator(
+    eval_pairs,
+    eval_labels,
+    name="scidocs_cite_eval",
 )
 
 # NOTE: Cached Version is slower to train and the batch size is sufficient to not benefit from reducing GPU memory usage
@@ -41,6 +69,8 @@ args = CrossEncoderTrainingArguments(
     dataloader_num_workers=4,
     save_steps=0.25,  # Save a checkpoint 4 times per epoch
     report_to=["tensorboard"],
+    eval_strategy="steps",
+    eval_steps=40,  # Evaluate every 40 steps
     logging_steps=20,
     logging_first_step=True,
 )
@@ -49,6 +79,7 @@ trainer = CrossEncoderTrainer(
     model=model,
     train_dataset=train_dataset,
     loss=loss,
+    evaluator=evaluator,
     args=args,
 )
 
