cf082ba8479cf0f9c150e7fa700a60e50da269a9
Matthias Bergmann <matthias.bergmann@student.tugraz.at>
2026-01-10 11:45:16 +0100

    stacked bar graphs

--- project/plotting.py
+++ project/plotting.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 import numpy as np
 import pathlib
 import evaluate
@@ -8,13 +9,29 @@ DISPLAY_NAMES = {
     "./models/cross_encoder/tinybert_scidocs_cite": "TinyBERT Fine Tuned",
     "sentence-transformers/allenai-specter": "Specter",
     "sentence-transformers/stsb-roberta-base-v2": "RoBERTa",
+    "./models/embedding/roberta_scidocs_cite": "RoBERTa Fine Tuned",
     "pritamdeka/S-Scibert-snli-multinli-stsb": "SciBERT",
     "relish": "Relish",
     "scidocs_cite": "SciDocs-Cite",
     "None": "None",
 }
 
-def plot_metric(dataset_name, dataset_results, metric_name, metric, save_path=None):
+METRIC_DISPLAY_NAMES = {
+    "P": "Precision",
+    "R": "Recall",
+    "nDCG": "nDCG",
+    "AP": "MAP",
+    "RR": "MRR",
+    "Success": "Success",
+}
+
+def plot_metric(dataset_name, dataset_results, metric, save_path=None):
+    metric_name = str(metric)
+    if '@' in metric_name:
+        metric_name = METRIC_DISPLAY_NAMES[metric_name.split('@')[0]] + "@" + metric_name.split('@')[1]
+    else:
+        metric_name = METRIC_DISPLAY_NAMES[metric_name]
+    
     fig, ax = plt.subplots(figsize=(10, 6))
     
     ax.set_title(f"{metric_name} on {DISPLAY_NAMES[dataset_name]} Dataset")
@@ -24,8 +41,8 @@ def plot_metric(dataset_name, dataset_results, metric_name, metric, save_path=No
     bar_width = 0.25
 
     for model_index, (model_name, model_results) in enumerate(dataset_results.items()):
-        for reranker_index, (reranker_name, metrics) in enumerate(model_results.items()):
-            value = metrics[metric]
+        for reranker_index, (reranker_name, metric_values) in enumerate(model_results.items()):
+            value = metric_values[metric]
             bar_position = x[model_index + 1] - (reranker_index - len(model_results)/2) * bar_width
 
             bar = ax.barh(bar_position, value, height=bar_width, label=DISPLAY_NAMES[reranker_name] if model_index == 0 else "", color=f"C{reranker_index}")
@@ -45,19 +62,90 @@ def plot_metric(dataset_name, dataset_results, metric_name, metric, save_path=No
     
     plt.show()
 
+def plot_metrics(dataset_name, dataset_results, metrics, save_path=None):
+    metric_names = []
+    for metric in metrics:
+        metric_name = str(metric)
+        if '@' in metric_name:
+            metric_name = METRIC_DISPLAY_NAMES[metric_name.split('@')[0]] + "@" + metric_name.split('@')[1]
+        else:
+            metric_name = METRIC_DISPLAY_NAMES[metric_name]
+        metric_names.append(metric_name)
+    metric_name = metric_names[-1].split('@')[0]
+    
+    fig, ax = plt.subplots(figsize=(10, 6))
+    
+    ax.set_title(f"{metric_name} on {DISPLAY_NAMES[dataset_name]} Dataset")
+    ax.set_xlabel(f"{metric_name}")
+    
+    x = np.arange(len(dataset_results) + 3)
+    bar_width = 0.25
+
+    hatches = ["", "xx", "..", "//"]
+    alphas = [1.0, 0.7, 0.5, 0.3]
+
+    for model_index, (model_name, model_results) in enumerate(dataset_results.items()):
+        for reranker_index, (reranker_name, metric_values) in enumerate(model_results.items()):
+            values = [metric_values[metric] for metric in metrics]
+            bar_position = x[model_index + 1] - (reranker_index - len(model_results)/2) * bar_width
+
+            bar = None
+            for metric_index, (value, metric_name) in enumerate(zip(values, metric_names)):
+                left = 0 if metric_index == 0 else values[metric_index - 1]
+                plt.rcParams["hatch.color"] = f"C{reranker_index}"
+                bar = ax.barh(
+                    bar_position,
+                    value - left,
+                    height=bar_width,
+                    label=DISPLAY_NAMES[reranker_name] if model_index == 0 and metric_index == 0 else "",
+                    color=f"C{reranker_index}",
+                    alpha=alphas[metric_index],
+                    # edgecolor=f"C{reranker_index + 1}" if metric_index > 0 else None,
+                    hatch=hatches[metric_index],
+                    left=left)
+                plt.rcParams["hatch.color"] = "black"
+
+            ax.bar_label(bar, labels=[f"{values[-1]:.3f}"], label_type="edge", padding=3)
+
+    legend1 = ax.legend(title="Rerankers", loc='upper left')
+
+    ax.legend(title="Metrics", loc='upper right', handles=[
+            Patch(
+                facecolor='white',
+                hatch=hatch,
+                edgecolor="black",
+                label=label
+            ) for hatch, label in zip(hatches[:len(metrics)], metric_names)
+        ])
+    ax.add_artist(legend1)
+    
+    ax.set_yticks(x)
+    ax.set_yticklabels([""] + [DISPLAY_NAMES[n] for n in dataset_results.keys()] + [""] * 2)           
+    ax.grid(axis='x') 
+    
+    # add x range padding
+    xmin, xmax = ax.get_xlim()
+    ax.set_xlim(xmax=xmax + 0.05 * (xmax - xmin))
+    
+    if save_path:
+        plt.savefig(str(save_path / f"{dataset_name}_{'_'.join(metric_names)}.png"))
+    
+    plt.show()
+
 
 if __name__ == "__main__":
-    save_path = pathlib.Path("figures")
-    save_path.mkdir(exist_ok=True)
-    
     eval_sets = {
         "relish": evaluate.relish_to_q_doc_qrel(evaluate.load_relish()["evaluation"].shard(20, 0)),
-        "scidocs_cite": evaluate.scidoc_cite_to_q_doc_qrel(
-            evaluate.load_scidocs_cite()["validation"].shard(100, 0)
-        ),
+        # "scidocs_cite": evaluate.scidoc_cite_to_q_doc_qrel(
+        #     evaluate.load_scidocs_cite()["validation"].shard(100, 0)
+        # ),
     }
-    
     eval_results = evaluate.evaluate_all(eval_sets)
     
+    save_path = pathlib.Path("figures")
+    save_path.mkdir(exist_ok=True)
+
     for dataset_name, dataset_results in eval_results.items():
-         plot_metric(dataset_name, dataset_results, "Success@5", evaluate.Success@5, save_path=save_path)
+        for metric in evaluate.METRICS_PER_DATASET[dataset_name]:
+             plot_metric(dataset_name, dataset_results, metric, save_path=save_path)
+        plot_metrics(dataset_name, dataset_results, [evaluate.Success@1, evaluate.Success@5, evaluate.Success@10], save_path=save_path)
